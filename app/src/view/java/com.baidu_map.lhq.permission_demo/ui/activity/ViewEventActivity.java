87ff87701c30e14b671212c3dafdabced24fa311
LHQ <caizhiqiang@abupdate.com>
2018-11-02 16:14:48 +0800

    scrollTo/scrollBy和LayoutParams

--- app/src/view/java/com.baidu_map.lhq.permission_demo/ui/activity/ViewEventActivity.java
+++ app/src/view/java/com.baidu_map.lhq.permission_demo/ui/activity/ViewEventActivity.java
@@ -22,6 +22,7 @@ public class ViewEventActivity extends Activity {
     GestureDetector mGestureListener;
     AnimationView mAnimationView;
     Button mButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
