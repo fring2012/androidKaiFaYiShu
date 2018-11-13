cbffff0846d4f2a840c3476ee842fd4f14991ecf
LHQ <caizhiqiang@abupdate.com>
2018-11-13 14:26:33 +0800

    1

--- app/src/view/java/com.baidu_map.lhq.permission_demo/ui/activity/ViewEventActivity.java
+++ app/src/view/java/com.baidu_map.lhq.permission_demo/ui/activity/ViewEventActivity.java
@@ -1,13 +1,18 @@
 package com.baidu_map.lhq.permission_demo.ui.activity;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Scroller;
@@ -23,7 +28,8 @@ public class ViewEventActivity extends Activity {
     GestureDetector mGestureListener;
     AnimationView mAnimationView;
     Button mButton;
-
+    //利用Handler实现弹性滑动,使用延时策略
+    ScrollHandler mScrollHandler;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +37,26 @@ public class ViewEventActivity extends Activity {
         mGestureListener = new GestureDetector(this,new GestureListener());
         mAnimationView = findViewById(R.id.animation_view);
         mButton = findViewById(R.id.btn_view_test);
-
+        mScrollHandler = new ScrollHandler();
 
         //scrollTo 和 scrollBy是对view的内容进行滑动
         //mAnimationView.scrollTo(-500,-100); //mAnimationView的内容向右滑动到500，100坐标处
 
         //通过改变view的LayoutParams的leftMargin的值，让view进行滑动
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams)mButton.getLayoutParams();
-        params.leftMargin += 100;
+//        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams)mButton.getLayoutParams();
+//        params.leftMargin += 100;
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                 //ObjectAnimator实现弹性滑动
+//                ObjectAnimator.ofFloat(mAnimationView,"translationX",0,100)
+//                        .setDuration(1000) //动画持续时间
+//                        .start();
+                //利用Handler延时策略实现弹性滑动,
+                mScrollHandler.sendEmptyMessage(mScrollHandler.MESSAGE_SCROLL_TO);
+            }
+        });
     }
 
     @Override
@@ -82,5 +100,34 @@ public class ViewEventActivity extends Activity {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         return super.dispatchTouchEvent(ev);
     }
+
+
+
+
+    @SuppressLint("HandlerLeak")
+    private class ScrollHandler  extends Handler {
+        private static final int MESSAGE_SCROLL_TO = 1;
+        private static final int FRAME_COUNT = 30;
+        private static final int DELAYED_TIME = 33;
+        private int mCount = 0;
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_SCROLL_TO: {
+                    mCount++;
+                    if (mCount <= FRAME_COUNT) {
+                        float fraction = mCount / (float) FRAME_COUNT;
+                        int scrollX = (int) (fraction * 100);
+                        mAnimationView.scrollTo(scrollX, 0);
+                        this.sendEmptyMessageDelayed(MESSAGE_SCROLL_TO,
+                                DELAYED_TIME);
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
+
 }
 
